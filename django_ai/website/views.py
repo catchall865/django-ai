4f1dcd75777a07b760daeefe183133a1d6e70514
Mark <markunder2022@protonmail.com>
2023-08-04 12:12:31 -0400

    authentication, history

--- django_ai/website/views.py
+++ django_ai/website/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from .models import Code
 import openai
 import os
 
@@ -32,6 +36,10 @@ def home(request):
                     frequency_penalty=0.0
                 )
                 response = (response['choices'][0]['text']).strip()
+
+                record = Code(question=code, code_answer=response, language=lang, user=request.user)
+                record.save()
+
                 return render(request, 'home.html', {'lang_list': lang_list, 'lang': lang, 'response': response})
             except Exception as e:
                 return render(request, 'home.html', {'lang_list': lang_list, 'response': e, 'lang': lang})
@@ -66,6 +74,10 @@ def suggest(request):
                     frequency_penalty=0.0
                 )
                 response = (response['choices'][0]['text']).strip()
+
+                record = Code(question=code, code_answer=response, language=lang, user=request.user)
+                record.save()
+
                 return render(request, 'suggest.html', {'lang_list': lang_list, 'lang': lang, 'response': response})
             except Exception as e:
                 return render(request, 'suggest.html', {'lang_list': lang_list, 'response': e, 'lang': lang})
@@ -73,3 +85,55 @@ def suggest(request):
 
 
     return render(request, 'suggest.html', {'lang_list': lang_list})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request=request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully logged in.")
+            return redirect('home')
+        else:
+            messages.success(request, 'Error logging in. Please try again.')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+    
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been successfully logged out.')
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have successfully registered a new account.')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'register.html', {'form':form})
+
+def past(request):
+    if request.user.is_authenticated:
+        code = Code.objects.filter(user_id=request.user.id)
+        return render(request, 'past.html', {'code': code})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_past(request, Past_id):
+    past = Code.objects.get(pk=Past_id)
+    past.delete()
+    messages.success(request, 'Previous entry deleted.')
+    return redirect('past')
--- test.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ['API_KEY'] = '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'
